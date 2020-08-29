e7533906e6570ef821b05b08d89a948c11197c08
Helena Liliana Vaquera <helenalvp@gmail.com>
2020-08-29 02:52:11 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function takeANumber(katzDeliLine, newName) {
+    katzDeliLine.push(newName);
+  return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`;
+
+}
+
+function nowServing (katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    //there is a problem i have when it comes to determining what the first persons name is and taking it out. i know i can do it in one line too
+    return `Currently serving ${katzDeliLine.slice(1)}`;
+    katzDeliLine.shift();
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine (katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    //make a loop that adds a number next to the current peoples names
+  } else {
+    return "The line is currently empty.";
+  }
+}
